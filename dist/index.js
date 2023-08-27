8f0068611d5009d5d9a7038da0362a7f5e892bfa
Mathéo GIRARD <matheo@voyelle.fr>
2023-08-27 19:14:57 +0200

    Update index

--- dist/index.js
+++ dist/index.js
@@ -22,36 +22,25 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const github = __importStar(require("@actions/github"));
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const octokit = github.getOctokit(core.getInput('token'));
-            // Get repository name and owner
-            const owner = github.context.payload.repository.owner.login;
-            const repository = github.context.payload.repository.name;
-            // Get content of the README.md file
-            const response = yield octokit.rest.repos.getContent({
-                owner,
-                repo: repository,
-                path: ''
-            });
-            console.log(response.data);
-        }
-        catch (error) {
-            core.setFailed(error.message);
-        }
-    });
+async function run() {
+    try {
+        const octokit = github.getOctokit(core.getInput('token'));
+        // Get repository name and owner
+        const owner = github.context.payload.repository.owner.login;
+        const repository = github.context.payload.repository.name;
+        // Get content of the README.md file
+        const response = await octokit.rest.repos.getContent({
+            owner,
+            repo: repository,
+            path: ''
+        });
+        console.log(response.data);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
 run();
