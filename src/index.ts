158167a07a80f7a19bd98c2617b171bb5ab16000
Mathéo GIRARD <matheo@voyelle.fr>
2023-08-27 18:52:33 +0200

    Start project

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const run = async () => {
+    try {
+        const octokit = github.getOctokit(core.getInput('token'));
+        // Get repository name and owner
+        const owner = github.context.payload.repository.owner.login;
+        const repository = github.context.payload.repository.name;
+        // Get content of the README.md file
+        const response = await octokit.rest.repos.getContent({
+            owner,
+            repo: repository,
+            path: ''
+        });
+        console.log(response.data);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+};
+
+await run();
